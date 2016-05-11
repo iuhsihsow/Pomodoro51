22746fdf8495f78239a65cfa70a2f6173f122c78
ben7276 <btan@esri.com>
2016-05-11 13:02:45 +0800

    add s2 for pause

--- src/pomodoro51.c
+++ src/pomodoro51.c
@@ -12,6 +12,7 @@ uint rest_counter = 300;
 uint status = 0; // 0 for  work, 1 for rest
 uint current_counter = 0;
 uint beep_times = 2;
+uint pause = 0; // 1 for pause, 0 for work
 
 
 unsigned char code wei[]=
@@ -52,6 +53,19 @@ void delay(uint z)
 	uint x,y;
 	for(x = z; x > 0; x--)
 		for(y = 114; y > 0 ; y--);
+}
+
+void Beeeeeep(uint times)
+{
+  uint total = 0;
+  while( total < times )
+  {
+   		total ++;
+		beep = ~beep;
+		delay(200);
+		beep = ~beep;
+		delay(800);
+  }
 }
 
 void display(uint i)
@@ -164,20 +178,63 @@ void displayLow(uint i)
 	delay(1);
 }
 
-void Beeeeeep(uint times)
+
+uchar KeyScan()	//带返回值的子函数
 {
-  uint total = 0;
-  while( total < times )
-  {
-   		total ++;
-		beep = ~beep;
-		delay(200);
-		beep = ~beep;
-		delay(800);
-
-  }
-
+	uchar cord_l,cord_h;//声明列线和行线的值的储存变量
+	P3 = 0x0f;//0000 1111
+	if( (P3 & 0x0f) != 0x0f)//判断是否有按键按下
+	{
+		delay(5);//软件消抖
+		if( (P3 & 0x0f) != 0x0f)//判断是否有按键按下
+		{
+			  cord_h = P3 & 0x0f;// 储存行线值
+			  P3 = cord_l | 0xf0;
+			  cord_l = P3 & 0xf0;// 储存列线值
+			  while( (P3 & 0xf0) != 0xf0 );//松手检测
+			  return (cord_l + cord_h);//返回键值码
+		}	
+	}
+		
 }
+
+// Test keys
+void KeyPro()
+{
+	switch( KeyScan() )
+	{
+	 	//第一行键值码
+		case 0xee: P0 = leddata[0];		break;
+		case 0xde: P0 = leddata[1];		break;
+		case 0xbe: P0 = leddata[2];		break;
+		case 0x7e: P0 = leddata[3];		break;
+		
+		//第二行键值码
+		case 0xed: P0 = leddata[4];		break;
+		case 0xdd: P0 = leddata[5];		break;
+		case 0xbd: P0 = leddata[6];		break;
+		case 0x7d: P0 = leddata[7];		break;
+
+		//第三行键值码
+		case 0xeb: P0 = leddata[8];		break;
+		case 0xdb: P0 = leddata[9];		break;
+		case 0xbb: P0 = leddata[10];	break;
+		case 0x7b: P0 = leddata[11];	break;
+
+		//第四行键值码
+		case 0xe7: P0 = leddata[12];	break;
+		case 0xd7: P0 = leddata[13];	break;
+		case 0xb7: P0 = leddata[14];	break;
+		case 0x77: P0 = leddata[15];	break;
+		//独立键盘
+		case 0xfe: P0 = leddata[16];	break;
+		case 0xfd: P0 = leddata[17];	break;
+		case 0xfb: P0 = leddata[18];	break;
+		case 0xf7: P0 = leddata[19];	break;
+	}	
+}
+
+
 
 void main()
 {
@@ -189,14 +246,29 @@ void main()
 
 	while(1)
 	{
-		if(TF0 == 1)
+		uchar key = KeyScan();
+		if(key == 0xfe)
+		{
+			Beeeeeep(1);
+			if(pause == 0)
+			{
+				pause = 1;
+			}
+			else if(pause == 1)
+			{
+				pause = 0;
+			}
+		}
+
+
+		if(TF0 == 1 && pause == 0)
 		{
 			TF0 = 0;
 			TH0 = 0x4b;
 			TL0 = 0xfe; //定时50ms
 			counter++;
 		}
-		if(counter == 20)
+		if(counter == 20 && pause == 0)
 		{
 			counter = 0;
 			//LED1 = ~LED1;
